d86f3222bf31f11388b671c7db94af87d4788871
HELLOWORLDYAN <2847808109@qq.com>
2017-07-25 11:35:50 +0800

    测试提交

---
+++ bbjob/bbjob_system/src/main/java/com/rundatop/sys/mapper/ArticleInfoMapper.java
@@ -0,0 +1,24 @@
+package com.rundatop.sys.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import tk.mybatis.mapper.common.Mapper;
+
+import com.rundatop.sys.example.ArticleInfoExample;
+import com.rundatop.sys.model.ArticleColumn;
+import com.rundatop.sys.model.ArticleInfo;
+import com.rundatop.sys.model.ModelAttribute;
+import com.rundatop.sys.search.ArticleInfoSearch;
+
+
+public interface ArticleInfoMapper extends Mapper<ArticleInfo>{
+	
+	List<ArticleInfo> getArtListByExample(@Param("example") ArticleInfoExample example, @Param("colId") String colId);
+	List<ArticleInfo> selNotinColArticle(@Param("columnId") String columnId,@Param("channelId") String channelId,@Param("name")String name,@Param("colId")String colId, @Param("begin")Integer begin, @Param("size")int size);
+	Integer selInColArticleCount(@Param("columnId") String columnId,@Param("channelId") String channelId,@Param("name")String name,@Param("colId")String colId, @Param("begin")Integer begin, @Param("size")int size);
+	Integer artNumCount(@Param("columnId") String columnId,@Param("channelId") String channelId,@Param("name")String name,@Param("colId")String colId, @Param("begin")Integer begin, @Param("size")int size);
+
+}
