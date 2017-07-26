1536698a345653d942596ae720af3623f794c2bd
HELLOWORLDYAN <2847808109@qq.com>
2017-07-26 08:14:24 +0800

    route，lazyload

--- bbjob/ng-framework-seed/src/main/java/freemarkerUtil/FreeMarkerUtil.java
+++ bbjob/ng-framework-seed/src/main/java/freemarkerUtil/FreeMarkerUtil.java
@@ -1,8 +1,7 @@
 package freemarkerUtil;
 
 import java.io.IOException;
-import java.io.StringWriter;
-import java.util.HashMap;
+import java.io.PrintWriter;
 import java.util.Map;
 
 import freemarker.template.Configuration;
@@ -14,8 +13,8 @@ public class FreeMarkerUtil {
 	public Template getTemplate(String name) {
 		try {
 			// 通过Freemaker的Configuration读取相应的ftl
-			Version version = new Version(2 * 1000000 + 3 * 1000 + 26);
-			Configuration cfg = new Configuration(version);
+			// Version version = new Version(2 * 1000000 + 3 * 1000 + 26);
+			Configuration cfg = new Configuration();
 
 	        cfg.setDefaultEncoding("UTF-8");  
 			// 设定去哪里读取相应的ftl模板文件
@@ -33,28 +32,16 @@ public class FreeMarkerUtil {
 	 * 输出到控制台
 	 * 
 	 */
-	public String print(String name, Map<String, Object> root) {
-		String result = "";
+	public void print(String name, Map<String, Object> root, PrintWriter out) {
 		try {
-			StringWriter writer = new StringWriter();
 			Template temp = this.getTemplate(name);
 			temp.setEncoding("UTF-8");  
-			temp.process(root, writer);
-			result = writer.toString();
-			writer.close();
+			temp.process(root, out);
 		} catch (TemplateException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		return result;
 	}
 
-	public static void main(String[] args) {
-		Map<String, Object> root = new HashMap<String, Object>();
-		root.put("username", "张三");
-		FreeMarkerUtil fmu = new FreeMarkerUtil();
-		System.out.println(fmu.print("test.ftl", root));
-	}
 }
