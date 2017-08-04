238fd2bcd4cb2902625f09d108721d0ea997511a
HELLOWORLDYAN <2847808109@qq.com>
2017-08-04 09:34:53 +0800

    视频分支

--- bbjob/ng-framework-seed/src/main/java/com/rundatop/biz/boardModel/service/CommonLabelFactoryService.java
+++ bbjob/ng-framework-seed/src/main/java/com/rundatop/biz/boardModel/service/CommonLabelFactoryService.java
@@ -1,7 +1,9 @@
 package com.rundatop.biz.boardModel.service;
 
 import java.beans.IntrospectionException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,10 @@ public class CommonLabelFactoryService {
 	@Autowired
 	private PicArtListService artList;
 	
+	@Autowired
+	private VideoListService videoList;
+	
+	
 	/**
 	 * @param modelAttribute
 	 * @return 
@@ -27,8 +33,30 @@ public class CommonLabelFactoryService {
 		if ("1".equals(modelAttribute.getFlatBit())) { // 图文列表
 			
 			returnMap = artList.getAllData(modelAttribute);
-		} else {
-			// TODO
+		} else if ("2".equals(modelAttribute.getFlatBit())) {
+			
+			returnMap = videoList.getAllData(modelAttribute);
+/*			Map<String, Object> confMap = new HashMap<String, Object>();
+			confMap.put("flatBit", modelAttribute.getFlatBit());
+			
+			HashMap<String, String> dataItem = null;
+			
+			List<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+			
+			dataItem = new HashMap<String, String>();
+			dataItem.put("uu", "cfd9191aeb");
+			dataItem.put("vu", "79ff602f42");
+			dataItem.put("player", "player1");
+			data.add(dataItem);
+			
+			dataItem = new HashMap<String, String>();
+			dataItem.put("uu", "cfd9191aeb");
+			dataItem.put("vu", "79ff602f42");
+			dataItem.put("player", "player2");
+			data.add(dataItem);
+			
+			returnMap.put("conf", confMap);
+			returnMap.put("data", data);*/
 		}
 		
 		return returnMap;
